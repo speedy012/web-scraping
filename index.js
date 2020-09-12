70083fa82680de657180204e0e839ed882fc2ecf
Herman Rivas <hrivas07@gmail.com>
2020-09-12 17:39:44 -0400

    newFile

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-'new file';
+const axios = require('axios');
+
+const page_url = 'https://www.newbalance.com/';
+
+async function getNew() {
+  const { data } = await axios.get(page_url);
+  console.log(data);
+}
+
+getNew();
