133b9946b1e158ee805e891a84b0669135806914
Herman Rivas <hrivas07@gmail.com>
2020-09-23 22:59:11 -0400

    info

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const axios = require('axios');
+const cheerio = requiew('cheerio');
 
 const page_url = 'https://www.newbalance.com/';
 
 async function getNew() {
   const { data } = await axios.get(page_url);
+  const $ = cheerio.load(data);
   console.log(data);
 }
 
-getNew();
+getNew(data);
+
+//hits api and returns data
